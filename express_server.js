ca417c7e7333a8fa948a33eefaae5b4a46965756
Natasha Chiu <chiuchiu07@gmail.com>
2023-07-11 01:58:27 +0800

    added EJS as template engine

--- express_server.js
+++ express_server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// use EJS as template engine
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get('/', (request, response) => {
   response.send('Hello!');
