f1be69cd2b39e37ee9978868b7ee25da23f76911
Natasha Chiu <chiuchiu07@gmail.com>
2023-07-11 04:40:35 +0800

    POST route for form submission

--- express_server.js
+++ express_server.js
@@ -5,6 +5,9 @@ const PORT = 8080;
 // use EJS as template engine
 app.set('view engine', 'ejs');
 
+// make POST request body human-readable
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,6 +18,16 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+// accept different route parameters
 app.get('/urls/:id', (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -22,6 +35,20 @@ app.get('/urls/:id', (req, res) => {
   };
   res.render('urls_show', templateVars);
 });
+
+const generateRandomString = function() {
+  let randomStr = '';
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const length = 6;
+
+  for (let i = 0; i < length; i++) {
+    const randomChar = chars.charAt(Math.floor(Math.random() * chars));
+    randomStr += randomChar;
+  }
+
+  return randomStr;
+};
+
 
 
 app.get('/', (request, response) => {
